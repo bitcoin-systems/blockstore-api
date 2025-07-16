58f279079930af15d10d30258045f92239da7d3e
ahsan-javaid <ahsan786cs@gmail.com>
2025-07-16 12:27:33 +0500

    fix: updates

--- api/scripts/gsat.ts
+++ api/scripts/gsat.ts
@@ -1,49 +1,45 @@
 export function printGsat(items) {
-
   const final: any = [];
-  const filtered: any = items.filter((e) => e.isverified === 'true' && e.matchedtx);
 
-  for (const tx of filtered) {
+  for (const tx of items) {
+    const amount = tx.tokenvalueusd;
 
-    if (tx) {
+    let tickets = 0;
 
-      const amount = tx.tokenvalueusd; 
-      let tickets = 0;
-
-      if (tx.txtype === 'bridge') {
-        tickets = 0;
-        if (amount >= 40) {
-          // bonus
-          tickets += 3;
-        }
+    if (tx.txtype === 'bridge') {
+      tickets = 0;
+      if (amount >= 40) {
+        // bonus
+        tickets += 3;
       }
-
-      if (tx.txtype === 'lend' || tx.txtype === 'swap') {
-        if (amount >= 10 && amount <= 100) {
-          tickets = 1;
-        } else if (amount >= 100 && amount <= 500) {
-          tickets = 3;
-        } else if (amount > 500) {
-          tickets = 5;
-        }
-      }
-
-      if (tx.tokenname === '243') {
-        tx.tokenvalue = tx.tokenvalue / 10 ** 6;
-        tx.tokenname = 'RBTC';
-      }
-
-      final.push({
-        address: tx.address,
-        tickets: tickets,
-        amount: amount,
-        token: `${String(tx.tokenvalue).slice(0, 8)} ${tx.tokenname}`,
-        tx: tx.matchedtx,
-        site: tx.partner,
-        op: tx.txtype,
-        date: tx.datetime
-      });
     }
+
+    if (tx.txtype === 'lend' || tx.txtype === 'swap') {
+      if (amount >= 10 && amount <= 100) {
+        tickets = 1;
+      } else if (amount >= 100 && amount <= 500) {
+        tickets = 3;
+      } else if (amount > 500) {
+        tickets = 5;
+      }
+    }
+
+    if (tx.tokenname === '243') {
+      tx.tokenvalue = tx.tokenvalue / 10 ** 6;
+      tx.tokenname = 'RBTC';
+    }
+
+    final.push({
+      address: tx.address,
+      tickets: tickets,
+      amount: amount,
+      token: `${String(tx.tokenvalue).slice(0, 8)} ${tx.tokenname}`,
+      tx: tx.id,
+      site: tx.partner,
+      op: tx.txtype,
+      phase: tx.phase,
+      date: tx.datetime
+    });
   }
 
   let data = `
@@ -103,6 +99,7 @@ export function printGsat(items) {
         <th>Amount</th>
         <th>Token</th>
         <th>Tx Hash</th>
+        <th>Phase</th>
         <th>Site</th>
         <th>Date</th>
       </tr>
@@ -112,6 +109,7 @@ export function printGsat(items) {
         <td>$${it.amount}</td>
         <td>${it.token}</td>
         <td> <a target="_blank" href="https://rootstock.blockscout.com/tx/${it.tx}"> ${it.op} </a></td>
+        <td>${it.phase}</td>
         <td>${it.site}</td>
         <td>${it.date}</td>
       </tr>`))}
